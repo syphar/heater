af4a4f039923536a0b526888004f1eb7f5d387fc
Denis Cornehl <denis.cornehl@gmail.com>
2021-01-21 23:58:06 +0100

    parallal

--- src/heater.rs
+++ src/heater.rs
@@ -9,24 +9,27 @@ pub enum HeatingError {
     RequestError(#[from] reqwest::Error),
 }
 
-pub async fn heat<T: IntoUrl>(iter: impl Iterator<Item = T>) -> Result<(), HeatingError> {
+pub async fn heat<T: 'static + IntoUrl + Send>(
+    iter: impl Iterator<Item = T>,
+) -> Result<(), HeatingError> {
     let client = Client::new();
 
     let bodies = stream::iter(iter)
         .map(|url| {
-            let client = &client;
-            async move {
+            let client = client.clone();
+            tokio::spawn(async move {
                 let resp = client.get(url).send().await?;
                 resp.bytes().await
-            }
+            })
         })
         .buffer_unordered(num_cpus::get());
 
     bodies
         .for_each(|b| async {
             match b {
-                Ok(b) => println!("Got {} bytes", b.len()),
-                Err(e) => eprintln!("Got an error: {}", e),
+                Ok(Ok(b)) => println!("Got {} bytes", b.len()),
+                Ok(Err(e)) => eprintln!("Got a reqwest::Error: {}", e),
+                Err(e) => eprintln!("Got a tokio::JoinError: {}", e),
             }
         })
         .await;
